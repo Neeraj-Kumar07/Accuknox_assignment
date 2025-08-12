c967de88f3e3c237bc302583faab4cfbcb6f6e35
Neeraj-Kumar07 <neerajkumar040403@gmail.com>
2025-08-12 12:19:33 +0530

    Initial commit

---
+++ app/consumers.py
@@ -0,0 +1,41 @@
+from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.exceptions import StopConsumer
+from time import sleep
+class MySyncConsumer (SyncConsumer):
+    def websocket_connect(self, event):
+        print('Websocket Connected...', event) 
+        self.send({ 'type': 'websocket.accept', })
+
+    def websocket_receive(self, event):
+        print('Message received from Client', event)
+        print(event['text'])
+        for i in range(30):
+            self.send({
+                'type': 'websocket.send',
+                'text': str(i)
+
+            })
+            sleep(1)
+
+    def websocket_disconnect(self, event):
+        print('Websocket Disconnected...', event)
+        raise StopConsumer()
+
+class MyAsyncConsumer (AsyncConsumer):
+    async def websocket_connect(self, event):
+        print('Websocket Connected...', event)
+        await self.send({
+            'type': 'websocket.accept',
+        })
+
+async def websocket_receive(self, event):
+    print('Message received from Client', event)
+    print(event['text'])
+    await self.send({
+        'type': 'websocket.send',
+        'text': 'Message Sent to Client'
+    })  
+
+async def websocket_disconnect(self, event):
+    print('Websocket Disconnected...', event) 
+    raise StopConsumer()
